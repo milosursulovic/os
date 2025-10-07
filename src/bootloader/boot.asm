998eb7546ba58b4f0e73df2886b56e6e238aa11c
Milos Ursulovic <milosursulovic2696@gmail.com>
2025-10-07 15:17:58 +0200

    Reading from disk in bios

--- src/bootloader/boot.asm
+++ src/bootloader/boot.asm
@@ -67,12 +67,93 @@ main:
 	mov ss, ax
 
 	mov sp, 0x7c00
+
+	mov [ebr_drive_number], dl
+	mov eax, 1
+	mov cl, 1
+	mov bx, 0x7e00
+	call disk_read
+
 	mov si, os_boot_msg
 	call print
 	hlt
 
 halt:
 	jmp halt
+
+; input: LBA index in ax
+; cx [bits0-5]: sector number
+; cx [bits 6-15]: cylinder
+; dh: head
+lba_to_chs:
+	push ax
+	push dx
+
+	xor dx, dx
+	div word [bdb_sectors_per_track] ; (LBA % sectors per track) + 1 <- sector
+	inc dx ; Sector
+	mov cx, dx
+
+	xor dx, dx
+	div word [bdb_heads]
+
+	mov dh, dl ; head (LBA / sectors per track) % number of heads
+	mov ch, al
+	shl ah, 6
+	or cl, ah ; cylinder: (LBA / sectors per track) / number of heads
+
+	pop ax
+	mov dl, al
+	pop ax
+
+	ret
+
+disk_read:
+	push ax
+	push bx
+	push cx
+	push dx
+	push di
+
+	call lba_to_chs
+
+	mov ax, 02h
+	mov di, 3
+
+retry:
+	stc
+	int 13h
+	jnc doneRead
+
+	call diskReset
+
+	dec di
+	test di, di
+	jnz retry
+
+failDiskRead:
+	mov si, read_failure
+	call print
+	hlt
+	jmp halt
+
+diskReset:
+	pusha
+	mov ah, 0
+	stc
+	int 13h
+	jc failDiskRead
+	popa
+	ret
+
+doneRead:
+	pop di
+	pop dx
+	pop cx
+	pop dx
+	pop ax
+
+	ret
 
 print:
 	push si
@@ -98,6 +179,9 @@ done_print:
 
 os_boot_msg:
 	db "OS has booted!",0x0d,0x0a,0
+
+read_falure:
+	db "Failed to read disk!",0x0d,0x0a,0
 
 times 510-($-$$) db 0
 dw 0aa55h
